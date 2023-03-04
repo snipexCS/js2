0394e141fdeb428d48adf4e3015ab9947e731cd3
snipexCS <123857302+snipexCS@users.noreply.github.com>
2023-03-04 16:30:14 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+let number1= +prompt("Введите первое число");
+let number2= +prompt("Введите второе число");
+let number3= +prompt("Введите третье число");
+alert( (number1 + number2 + number3) / 3);
